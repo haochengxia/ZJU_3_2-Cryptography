9f1818b074f08acdf204fd0b7f7631770243c1b3
haochengxia <3170102492@zju.edu.cn>
2020-04-22 21:34:23 +0800

    add id

--- hw1_sm4.py
+++ hw1_sm4.py
@@ -4,6 +4,9 @@
 import time
 from pysm4 import encrypt, decrypt, get_round_keys, reduction_mk, encrypt_ecb, decrypt_ecb
 
+print("+--------------------------+")
+print("|       key schedule       |")
+print("+--------------------------+")
 clear_num = 0x0123456789abcdeffedcba9876543210
 mk = 0x0123456789abcdeffedcba9876543210
 
@@ -23,6 +26,9 @@ print(flag)
 print(hex(re_mk))
 
 # Part 3 test 4 billion words
+print("+--------------------------+")
+print("|     encryption process   |")
+print("+--------------------------+")
 one_word = 'word'
 flag = 1
 count = 0
@@ -33,16 +39,16 @@ Test one unit
 '''
 print('one unit: ')
 print(decrypt_ecb((encrypt_ecb(unit_words, key)), key) == unit_words)
-start = time.process_time()
-for i in range(0,1000000000):
-    count = count + 1
-    if count%10000000==0:
-        print(count/10000000)
-    #hash_plain_text = hash(four_billion_words)
-    cipher_text = encrypt_ecb(unit_words, key)
-    #hash_decrypt_text = hash(decrypt_ecb(cipher_text, key))
-    decrypt_text = decrypt_ecb(cipher_text, key)
-    flag = ((decrypt_text == unit_words) and flag)
-print(flag)
-elapsed = (time.process_time() - start)
-print('time cost', elapsed)
+# start = time.process_time()
+# for i in range(0,1000000000):
+#     count = count + 1
+#     if count%10000000==0:
+#         print(count/10000000)
+#     #hash_plain_text = hash(four_billion_words)
+#     cipher_text = encrypt_ecb(unit_words, key)
+#     #hash_decrypt_text = hash(decrypt_ecb(cipher_text, key))
+#     decrypt_text = decrypt_ecb(cipher_text, key)
+#     flag = ((decrypt_text == unit_words) and flag)
+# print(flag)
+# elapsed = (time.process_time() - start)
+# print('time cost', elapsed)
